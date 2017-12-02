44450de4e32c42f435008a998046ae37c139a906
vim <vladimir.moiseiev@teamdev.com>
2017-12-02 12:20:02 +0200

    Query factory

--- src/query_factory.cc
+++ src/query_factory.cc
@@ -3,3 +3,44 @@
 //
 
 #include "query_factory.h"
+
+#include <sstream>
+
+
+
+#include "actor_request_factory.h"
+
+namespace spine {
+namespace client {
+
+const std::string QUERY_ID_TEMPLATE = "query-";
+
+QueryFactory::QueryFactory(const ActorRequestFactory &actor_request_factory)
+{
+    actor_context_ = actor_request_factory.actor_context();
+}
+
+std::unique_ptr<Query> QueryFactory::all(const std::string& type_url) {
+    Target *target = Target::default_instance().New();
+    target->set_type(type_url);
+
+    std::unique_ptr<Query> query = Query::default_instance().New();
+
+    query->set_allocated_id(createQueryId());
+
+    query->set_allocated_context(new ::spine::core::ActorContext(*actor_context_));
+    query->set_allocated_target(target);
+
+    return query;
+}
+
+QueryId *QueryFactory::createQueryId() {
+    QueryId *query_id = new QueryId();
+    std::__1::stringstream query_id_stream;
+    query_id_stream << QUERY_ID_TEMPLATE << uuid_generator_.createRandom().toString();
+    query_id->set_value(query_id_stream.str());
+    return query_id;
+}
+
+
+}} // namespace
--- src/query_factory.h
+++ src/query_factory.h
@@ -5,11 +5,35 @@
 #ifndef SPINE_QUERYFACTORY_H
 #define SPINE_QUERYFACTORY_H
 
+#include <memory>
+#include <Poco/UUIDGenerator.h>
+#include <spine/client/query.pb.h>
+
 namespace spine {
+
+namespace core {
+    class ActorContext;
+}
+
 namespace client {
+
+class ActorRequestFactory;
+class Query;
 
 class QueryFactory
 {
+public:
+    QueryFactory(const ActorRequestFactory & actor_request_factory);
+
+public:
+    std::unique_ptr<Query> all(const std::string& type_url);
+
+private:
+    spine::client::QueryId *createQueryId();
+private:
+    std::unique_ptr<core::ActorContext> actor_context_;
+    Poco::UUIDGenerator uuid_generator_;
+
 
 };
 
