88225fbc6c445b98cd4be8df84df812e201bbc8f
vim <vladimir.moiseiev@teamdev.com>
2018-01-13 17:25:24 +0200

    actor request factory formatting

--- src/actor_request_factory.cc
+++ src/actor_request_factory.cc
@@ -23,19 +23,22 @@
 #include "spine/topic_factory.h"
 #include "spine/command_factory.h"
 #include "spine/query_factory.h"
+#include "spine/message_utils.hpp"
 
 #include <google/protobuf/util/time_util.h>
 
 using namespace spine;
 using namespace spine::client;
+using namespace spine::time;
 using namespace google::protobuf;
 using namespace google::protobuf::util;
 
-spine::core::ActorContext *
-create_actor_context(spine::core::UserId* actor, spine::time::ZoneOffset* offset, spine::core::TenantId* tenant_id,
-                     google::protobuf::Timestamp* timestamp)
+ActorContext* create_actor_context(UserId* actor,
+                                                ZoneOffset* offset,
+                                                TenantId* tenant_id,
+                                                Timestamp* timestamp)
 {
-    spine::core::ActorContext *actor_context = spine::core::ActorContext::default_instance().New();
+    ActorContext *actor_context = ActorContext::default_instance().New();
     actor_context->set_allocated_timestamp(timestamp);
     actor_context->set_allocated_actor(actor);
     actor_context->set_allocated_zone_offset(offset);
@@ -48,33 +51,34 @@ ActorRequestFactory::ActorRequestFactory(const ActorRequestFactoryParams& params
 {
     if (!params.zone_offset())
     {
-        params_.set_zone_offset(std::make_unique<spine::time::ZoneOffset>(spine::time::ZoneOffset::default_instance()));
+        params_.set_zone_offset(std::make_unique<ZoneOffset>(ZoneOffset::default_instance()));
     }
 }
 
-std::unique_ptr<CommandFactory> ActorRequestFactory::command_factory()
+CommandFactoryPtr ActorRequestFactory::command_factory()
 {
     return std::make_unique<CommandFactory>(*this);
 }
 
-std::unique_ptr<TopicFactory> ActorRequestFactory::topic_factory()
+TopicFactoryPtr ActorRequestFactory::topic_factory()
 {
     return std::make_unique<TopicFactory>(*this);
 }
 
-std::unique_ptr<QueryFactory> ActorRequestFactory::query_factory()
+QueryFactoryPtr ActorRequestFactory::query_factory()
 {
     return std::make_unique<QueryFactory>(*this);
 }
 
-std::unique_ptr<core::ActorContext> ActorRequestFactory::actor_context() const
+std::unique_ptr<ActorContext> ActorRequestFactory::actor_context() const
 {
     Timestamp* timestamp_ptr = Timestamp::default_instance().New();
     timestamp_ptr->CopyFrom(TimeUtil::GetCurrentTime());
-    std::unique_ptr<core::ActorContext> actor_context{
-            create_actor_context(new core::UserId(*params_.actor()),
-                                 new time::ZoneOffset(*params_.zone_offset()),
-                                 new core::TenantId(*params_.tenant_id()),
+
+    std::unique_ptr<ActorContext> actor_context{
+            create_actor_context(clone(params_.actor()),
+                                 clone(params_.zone_offset()),
+                                 clone(params_.tenant_id()),
                                  timestamp_ptr)
     };
     return actor_context;
@@ -86,9 +90,9 @@ ActorRequestFactory ActorRequestFactory::create(const ActorRequestFactoryParams&
     return factory;
 }
 
-const std::unique_ptr<core::UserId>& ActorRequestFactory::actor() const { return params_.actor(); }
+const std::unique_ptr<UserId>& ActorRequestFactory::actor() const { return params_.actor(); }
 
-const std::unique_ptr<core::TenantId>& ActorRequestFactory::tenant_id() const { return params_.tenant_id(); }
+const std::unique_ptr<TenantId>& ActorRequestFactory::tenant_id() const { return params_.tenant_id(); }
 
-const std::unique_ptr<time::ZoneOffset>& ActorRequestFactory::zone_offset() const { return params_.zone_offset(); }
+const std::unique_ptr<ZoneOffset>& ActorRequestFactory::zone_offset() const { return params_.zone_offset(); }
 
