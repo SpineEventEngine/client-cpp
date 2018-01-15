692abfb8b6728532e4294b1b2bd6283ffa283ce3
vim <vladimir.moiseiev@teamdev.com>
2018-01-15 13:04:07 +0200

    remove dependency and build scripts that are not ready

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "GRPC in $1"
-
-mkdir -p .build
-cd .build
-
-cmake -DGRPC_INSTALL_DIR=$1 ..
-cmake --build . -- -j5
--- spine-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p dependencies/spine
-cd dependencies
-curl -o spine.jar http://maven.teamdev.com/repository/spine-snapshots/io/spine/spine-client/0.10.1-SNAPSHOT/spine-client-0.10.1-20171019.131949-2-proto.jar
-unzip spine.jar -d spine
