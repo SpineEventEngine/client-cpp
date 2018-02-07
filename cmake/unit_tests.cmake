a00633ec619b011dd504ec37bd811b87188f8a49
Vladimir Moiseiev <vladimir.moiseiev@teamdev.com>
2018-02-07 19:23:12 +0200

    make depdendency build optional

--- cmake/unit_tests.cmake
+++ cmake/unit_tests.cmake
@@ -1,30 +1,3 @@
-set(GTEST_DEPENDENCIES_DIR ${CMAKE_BINARY_DIR}/dependencies/gtest/)
-
-# Download and unpack googletest at configure time.
-configure_file(cmake/gtest_cmake.in ${GTEST_DEPENDENCIES_DIR}/googletest-download/CMakeLists.txt)
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${GTEST_DEPENDENCIES_DIR}/googletest-download )
-if(result)
-    message(FATAL_ERROR "CMake step for googletest failed: ${result}")
-endif()
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${GTEST_DEPENDENCIES_DIR}/googletest-download )
-if(result)
-    message(FATAL_ERROR "Build step for googletest failed: ${result}")
-endif()
-
-# Prevent overriding the parent project's compiler/linker
-# settings on Windows.
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-
-# Add googletest directly to our build. This defines
-# the gtest and gtest_main targets.
-add_subdirectory(${GTEST_DEPENDENCIES_DIR}/googletest-src
-        ${GTEST_DEPENDENCIES_DIR}/googletest-build
-        EXCLUDE_FROM_ALL)
-
 set(TEST_SRCS
         test/actor_request_factory_test.cc
         test/command_factory_test.cc
@@ -59,6 +32,8 @@ foreach(proto_file ${test_proto_files})
 endforeach(proto_file)
 
 add_executable(runTests ${TEST_SRCS} ${test_generated_files})
+
+add_subdirectory(${GTEST_DEPENDENCIES_DIR} EXCLUDE_FROM_ALL)
 
 target_link_libraries(runTests gtest_main ${CPP_SPINE_LIBRARY_NAME}
         ${PROTOBUF_LIB}
