67b15e99d9af48805909d41ac8a232a1122fad51
vim <vladimir.moiseiev@teamdev.com>
2018-03-13 03:19:39 +0200

    add target_version explanation

--- include/spine/command_factory.h
+++ include/spine/command_factory.h
@@ -51,7 +51,6 @@ namespace client
 * @see ActorRequestFactory::command_factory()
 *
 * @author Vladimir Moiseiev
- * @file
 */
 
 class CommandFactory
@@ -71,9 +70,25 @@ public:
      * Creates a command instance with the given message and the context
      * provided during the construction.
      *
+     * The @p target_version parameter defines the version of the entity which handles
+     * the resulting command. Note that the framework performs no validation of the target version
+     * before a command is handled. The validation may be performed by the user themselves instead.
+     *
      * @param message the command message.
-     * @param target_version @todo
+     * @param target_version the version of the entity for which this command is intended.
      * @return a new Command instance.
+     *
+     * @todo add a server side reference, and a link to QueryResponse, once the
+     * feature is ready.
+     *
+     * @note Aggregates and ProcessManagers are the versioned entities, which can handle commands.
+     * Each modification of their state leads to the version increment.
+     * At the same time the target version number can be used as a parameter for the Command,
+     * being a way to escape from updating the stale entities (see CommandContext.target_version attribute).
+     * I.e. the client may specify the expected version of the target entity,
+     * in which as he believes this entity resides. In case the actual entity version differs from the expected
+     * version set for the command, the command is rejected — as it clearly
+     * has been created with the stale entity state in mind.
      */
     CommandPtr create(const Message& message, int target_version);
 
