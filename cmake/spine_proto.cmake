b4074c661a1ea0e08bba4e7fc152740910f588a9
Vladimir Moiseiev <vladimir.moiseiev@teamdev.com>
2019-04-10 20:29:08 +0300

    fix build issues

--- cmake/spine_proto.cmake
+++ cmake/spine_proto.cmake
@@ -1,37 +1,37 @@
 
 set(spine_proto_files
-        spine/ui/color.proto
-        spine/ui/language.proto
-        spine/validate/validation_error.proto
+        spine/base/error.proto
+        spine/base/field_filter.proto
+        spine/base/field_path.proto
         spine/change/change.proto
         spine/change/value_mismatch.proto
-        spine/net/email_address.proto
-        spine/net/url.proto
-        spine/net/internet_domain.proto
+        spine/client/command_service.proto
+        spine/client/entities.proto
+        spine/client/filters.proto
+        spine/client/query.proto
+        spine/client/query_service.proto
+        spine/client/subscription.proto
+        spine/client/subscription_service.proto
+        spine/core/ack.proto
+        spine/core/actor_context.proto
+        spine/core/bounded_context.proto
+        spine/core/command.proto
+        spine/core/enrichment.proto
+        spine/core/event.proto
         spine/core/response.proto
         spine/core/tenant_id.proto
         spine/core/user_id.proto
-        spine/core/actor_context.proto
-        spine/core/bounded_context.proto
-        spine/core/ack.proto
-        spine/core/event.proto
         spine/core/version.proto
-        spine/core/command.proto
-        spine/core/enrichment.proto
-        spine/time/time_change.proto
-        spine/time/time.proto
-        spine/people/person_name.proto
-        spine/client/subscription.proto
-        spine/client/query_service.proto
-        spine/client/filters.proto
-        spine/client/query.proto
-        spine/client/entities.proto
-        spine/client/subscription_service.proto
-        spine/client/command_service.proto
-        spine/base/field_path.proto
-        spine/base/field_filter.proto
-        spine/base/error.proto
+        spine/net/email_address.proto
+        spine/net/internet_domain.proto
+        spine/net/url.proto
         spine/options.proto
+        spine/people/person_name.proto
+        spine/time/time.proto
+        spine/time/time_change.proto
+        spine/ui/color.proto
+        spine/ui/language.proto
+        spine/validate/validation_error.proto
         )
 
 set(GENERATED_DIR ${CMAKE_BINARY_DIR}/generated)
